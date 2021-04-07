9c56d820e61d13bc2279b3e3bc0997692dc6514a
amimista <77865981+amimista@users.noreply.github.com>
2021-04-07 17:11:10 -0600

    deleted the runtime idea

--- src/Exec.java
+++ src/Exec.java
@@ -3,15 +3,9 @@
  */
 
 // imports
-import java.lang.Runtime;
 
 public class Exec {
   public static void main(String[] strings) {
-    try {
-      Runtime.getRuntime().exec(new String[] {"cmd", "/K", "Start"});
-    } catch(Exception e) {
-      System.out.println("HEY BUDDY! ur doing something worng, can ya` not!?");
-      e.printStackTrace();
-    }
+    
   }
 }
