5494aedee3e5357d031aebb77fbbcae8497462e4
amimista <77865981+amimista@users.noreply.github.com>
2021-04-07 17:00:46 -0600

    Update Exec.java

--- src/Exec.java
+++ src/Exec.java
@@ -3,12 +3,15 @@
  */
 
 // imports
-import java.io.*;
-import java.awt.GraphicsEnvironment;
-import java.net.URISyntaxException;
+import java.lang.Runtime;
 
 public class Exec {
   public static void main(String[] strings) {
-    
+    try {
+      Runtime.getRuntime().exec(new String[] {"cmd", "/K", "Start"});
+    } catch(Exception e) {
+      System.out.println("HEY BUDDY! ur doing something worng, can ya` not!?");
+      e.printStackTrace();
+    }
   }
 }
